2e6e389dcf108cc5d6fadb034996cc95265c9f90
Anca <aannccaa@gmail.com>
2017-04-26 16:29:04 +0200

    Module1 update

--- src/CUBE.java
+++
@@ -1,9 +0,0 @@
-public class CUBE {
-public static void main (String [] arg)
-{double a,b;
-System.out.print("Donner un nombre : ");
-a=Lire.d();
-b=a*a*a;
-System.out.print("Son cube vaut : " + b);
-}
-}
