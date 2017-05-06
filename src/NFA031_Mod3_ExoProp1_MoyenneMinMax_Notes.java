2ebf4dab5bcb483cfd6db4a9690ee36985556947
anca <aannccaa@gmail.com>
2017-05-06 17:20:37 +0200

    update

--- src/NFA031_Mod3_ExoProp1_MoyenneMinMax_Notes.java
+++ src/NFA031_Mod3_ExoProp1_MoyenneMinMax_Notes.java
@@ -8,11 +8,11 @@ la moyenne maximale, la moyenne minimale et la moyenne générale de la classe.
 
 public class NFA031_Mod3_ExoProp1_MoyenneMinMax_Notes {
 	public static void main(String[] args) {
-		int coefMath = 2, coefInfo = 4, coefFra = 2, nbEleves = 3;
+		int coefMath = 2, coefInfo = 4, coefFra = 2, nbEleves = 2;
 		double noteMath, noteInfo, noteFra, moyenneEleve, sommeMoyennesEleves = 0, moyenneMinClasse = 20,
 				moyenneMaxClasse = 0, moyenneGenClasse = 0;
 
-		for (int i = 1; i <= nbEleves; i = i + 1) {
+		for (int i = 1; i <= nbEleves; i++) {
 			System.out.print("Donner la note en Mathématique pour l'eleve n° " + i + " : ");
 			noteMath = Lire.d();
 			System.out.print("Donner la note en Informatique pour l'eleve n° " + i + " : ");
@@ -35,9 +35,9 @@ public class NFA031_Mod3_ExoProp1_MoyenneMinMax_Notes {
 
 		}
 		moyenneGenClasse = sommeMoyennesEleves / nbEleves;
-		System.out.format("Moyenne générale de la classe %d eleves = %.2f \n", nbEleves, moyenneGenClasse);
-		System.out.format("Moyenne min. de la classe %d eleves = %.2f \n", nbEleves, moyenneMinClasse);
-		System.out.format("Moyenne max. de la classe %d eleves = %.2f \n", nbEleves, moyenneMaxClasse);
+		System.out.format("Moyenne générale de la classe ayant %d eleves = %.2f \n", nbEleves, moyenneGenClasse);
+		System.out.format("Moyenne min. de la classe ayant %d eleves = %.2f \n", nbEleves, moyenneMinClasse);
+		System.out.format("Moyenne max. de la classe ayant %d eleves = %.2f \n", nbEleves, moyenneMaxClasse);
 
 	}
 }
--- src/NFA031_Mod3_ExoProp2_Puissance.java
+++ src/NFA031_Mod3_ExoProp2_Puissance.java
@@ -8,15 +8,26 @@ public class NFA031_Mod3_ExoProp2_Puissance {
 	public static void main(String[] args) {
 		int a, b, puissance = 1;
 
-		System.out.print("Donner un entier positif (a) :");
-		a = Lire.i();
-		System.out.print("Donner un entier positif (b) :");
-		b = Lire.i();
+		do {
+			System.out.print("Donner un entier strictement positif (a>0) :");
+			a = Lire.i();
+			if (a <= 0) {
+				System.out.print("Choix incorrect. ");
+			}
+		} while (a <= 0);
+
+		do {
+			System.out.print("Donner un entier positif (b>=0) :");
+			b = Lire.i();
+			if (b < 0) {
+				System.out.print("Choix incorrect. ");
+			}
+		} while (b < 0);
 
 		for (int i = 1; i <= b; i++) {
 			puissance = puissance * a;
 		}
-		System.out.print("a^b = " + puissance);
+		System.out.format("a^b = %d^%d = %d", a,b,puissance);
 	}
 
 }
