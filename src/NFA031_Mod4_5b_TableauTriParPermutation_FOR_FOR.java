ebbc1ca2db79d354652b906c2cb9ab61d67b42cf
anca <aannccaa@gmail.com>
2017-05-15 22:54:29 +0200

    new + update

--- src/NFA031_Mod4_5b_TableauTriParPermutation_FOR_FOR.java
+++ src/NFA031_Mod4_5b_TableauTriParPermutation_FOR_FOR.java
@@ -7,8 +7,8 @@ public class NFA031_Mod4_5b_TableauTriParPermutation_FOR_FOR {
 		int[] T = { 5, 7, 3, 1, 6 };
 		longueurTableau = T.length;
 		System.out.print("[");
-		for (i = 0; i <= longueurTableau - 2; i++) {
-			for (j = i + 1; j <= longueurTableau - 1; j++) {
+		for (i = 0; i < longueurTableau - 1; i++) {
+			for (j = i + 1; j < longueurTableau; j++) {
 				if (T[i] > T[j]) {
 					temp = T[i];
 					T[i] = T[j];
--- src/NFA031_Mod4_ExoProp3_Tableau.java
+++ src/NFA031_Mod4_ExoProp3_Tableau.java
@@ -6,28 +6,28 @@ l’indice est fourni par l’utilisateur.
 c) Ecrire un code pour trier par ordre décroissant les entiers contenus dans une colonne dont
 l’indice est fourni par l’utilisateur .
 
-Indication : afin de ne pas modifier les données du tableau initial, il faut effectuer les tris sur
-des copies des données.
+Indication : afin de ne pas modifier les données du tableau initial, 
+il faut effectuer les tris sur des copies des données.
  */
 
 public class NFA031_Mod4_ExoProp3_Tableau {
 
 	public static void main(String[] args) {
-		int nbLignes = 2;
-		int nbCol = 2;
-		
-		int indiceLigne;
-		int indiceCol;
-		//int tab[][] = new int[nbLignes][nbCol];
-		int tab[][] = {
-				{3,2,5,7,0},
-				{1,4,3,2,8},
-				{3,2,5,1,9},
-				{1,4,3,2,6},
-		};
+		int nbLignes = 4;
+		int nbCol = 5;
+		int tab[][] = new int[nbLignes][nbCol];
+		// creer un vecteur pour la ligne à trier:
+		int tabLigne[] = new int[nbCol];
+		// creer un vecteur pour la col à trier:
+		int tabCol[] = new int[nbLignes];
 
-/*
-		//introduire valeurs tableau
+		int indiceLigne = 2;
+		int indiceCol = 4;
+
+		// test
+		//int tab[][] = { { 3, 2, 5, 7, 0 }, { 1, 4, 3, 2, 8 }, { 3, 2, 5, 1, 9 }, { 1, 4, 3, 2, 6 } };
+
+		// introduire valeurs tableau
 		for (int i = 0; i < nbLignes; i++) {
 			for (int j = 0; j < nbCol; j++) {
 				System.out.print("Donner un nombre entier pour le croissement de la ligne " + i + " et de la colonne "
@@ -35,8 +35,8 @@ public class NFA031_Mod4_ExoProp3_Tableau {
 				tab[i][j] = Lire.i();
 			}
 		}
-		
-		// print tableau introduit
+
+		// print tableau introduit:
 		System.out.println("La matrice est: ");
 		for (int i = 0; i < nbLignes; i++) {
 			for (int j = 0; j < nbCol; j++) {
@@ -45,13 +45,57 @@ public class NFA031_Mod4_ExoProp3_Tableau {
 			}
 			System.out.println();
 		}
-*/
-		
-			System.out.print("Donner l'indice de la ligne que vous voulez trier en ordre croissante : ");
-			indiceLigne = Lire.i();
-		
-			System.out.print("Donner l'indice de la col que vous voulez trier en ordre décroissante : ");
-			indiceCol = Lire.i();
+
+		System.out.print("Donner l'indice de la ligne que vous voulez trier en ordre croissante : ");
+		indiceLigne = Lire.i();
+		// remplir le vecteur correspondant a l'indiceLigne
+		for (int j = 0; j < nbCol; j++) {
+			tabLigne[j] = tab[indiceLigne][j];
+		}
+
+		// ordonner croissant le vecteur pour indiceLigne
+		for (int i = 0; i < nbCol - 1; i++) {
+			for (int j = i + 1; j < nbCol; j++) {
+				if (tabLigne[i] > tabLigne[j]) {
+					int temp = tabLigne[i];
+					tabLigne[i] = tabLigne[j];
+					tabLigne[j] = temp;
+				}
+			}
+
+		}
+
+		// afficher le vecteur trié pour indiceLigne
+		System.out.println("Voici la ligne d'indice " + indiceLigne + " triée en ordre croissante:");
+		for (int i = 0; i <= nbCol - 1; i++) {
+			System.out.print(tabLigne[i] + " ");
+		}
+		System.out.println();
+
+		// remplir le vecteur correspondant a l'indiceCol
+		System.out.print("Donner l'indice de la col que vous voulez trier en ordre décroissante : ");
+		indiceCol = Lire.i();
+		for (int i = 0; i < nbLignes; i++) {
+			tabCol[i] = tab[i][indiceCol];
+		}
+
+		// ordonner décroissant le vecteur pour l'indiceCol
+		for (int i = 0; i < nbLignes - 1; i++) {
+			for (int j = i + 1; j < nbLignes; j++) {
+				if (tabCol[i] < tabCol[j]) {
+					int temp = tabCol[i];
+					tabCol[i] = tabCol[j];
+					tabCol[j] = temp;
+				}
+			}
+		}
+
+		// afficher le vecteur trié pour indiceCol
+		System.out.println("Voici la colonne d'indice " + indiceCol + " triée en ordre decroissante:");
+		for (int i = 0; i < nbLignes; i++) {
+			System.out.println(tabCol[i] + " ");
+		}
+		System.out.println();
 
 	}
 
