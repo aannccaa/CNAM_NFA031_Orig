09aef2e0a7261a200843cf71f45d9bc195469114
anca <aannccaa@gmail.com>
2017-05-17 20:35:50 +0200

    update

--- src/NFA031_Mod5_Bb_Fonction_passerTableauEnParam_SommeEntiers.java
+++ src/NFA031_Mod5_Bb_Fonction_passerTableauEnParam_SommeEntiers.java
@@ -7,27 +7,41 @@ La fonction ‘somme’ reçoit ce tableau en paramètre et retourne la somme des élém
 public class NFA031_Mod5_Bb_Fonction_passerTableauEnParam_SommeEntiers {
 
 	public static void main(String[] args) {
-		int nombreEntiers=5;
+		int nombreEntiers = 3;
+		int nombreEntiers2 = 2;
 		int sommeEntiers;
-		int tab[] = new int[nombreEntiers];
+		int tab[] = lireTableau("tablou 1", nombreEntiers);
+		int tab2[] = lireTableau("tablou 2", nombreEntiers2);
+
+		sommeEntiers = calcul_somme_entiers_tableau(tab);
+		System.out.println("La somme des " + nombreEntiers + " entiers introduits est = " + sommeEntiers);
+		sommeEntiers = calcul_somme_entiers_tableau(tab2);
+		System.out.println("La somme des " + nombreEntiers + " entiers tableau 2 introduits est = " + sommeEntiers);
 		
-		for (int i=0; i<nombreEntiers; i++) {
-			System.out.print("Donner l'entier numéro: " + i +" : " );
-		tab[i] = Lire.i();
-		}
-		
-		sommeEntiers = calcul_somme_entiers_tableau (tab);
-		System.out.println("La somme des " + nombreEntiers + " entiers introduits est = " +sommeEntiers);
 	}
 
 	// fonction qui passe un tableau d'entiers en parametre
-	public static int calcul_somme_entiers_tableau (int T[]) {
-		int nombreEntiersTableau=5, sommeEntiersTableau=0;
-		for(int i=0; i<nombreEntiersTableau; i++) {
-			sommeEntiersTableau = sommeEntiersTableau + T[i];
+	public static int[] lireTableau(String nomTableau, int longueurTableau) {
+		int tableau[] = new int[longueurTableau];
+		if (nomTableau != null && nomTableau != "") {
+			System.out.println("Introduire la liste des entiers appellée  " + nomTableau);
+		}
+		
+		for (int i = 0; i < longueurTableau; i++) {
+			System.out.print("Donner l'entier numéro: " + (i + 1) + " : ");
+			tableau[i] = Lire.i();
+		}
+		return tableau;
+
+	}
+
+	public static int calcul_somme_entiers_tableau(int tableau[]) {
+
+		int sommeEntiersTableau = 0;
+		for (int i = 0; i < tableau.length; i++) {
+			sommeEntiersTableau = sommeEntiersTableau + tableau[i];
 		}
 		return sommeEntiersTableau;
 	}
-	
 
 }
