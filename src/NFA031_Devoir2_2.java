6f7bc7923a779520c941d49dc979dadd233e5cd0
Anca <aannccaa@gmail.com>
2017-05-23 17:10:29 +0200

    update

--- src/NFA031_Devoir2_2.java
+++ src/NFA031_Devoir2_2.java
@@ -1,15 +1,17 @@
 /*
-a) Ecrire un programme qui demande à un utilisateur de donner N caractères qui sont soit des chiffres , soit des lettres , N étant un entier fourni par l'utilisateur au début de l'exécution du programme . Ces caractères seront stockés dans un tableau 'tab' . 
-  b) Ecrire un code qui permet de compter le nombre nc de chiffres contenus dans ce tableau .
-  c) Soit nl le nombre de lettres contenues dans le tableau 'tab' . 
-Ecrire un code pour recopier les données contenues dans 'tab' dans un tableau 'tabbis' selon la règle suivante : 
-si nc ≥ nl , recopier les chiffres de 'tab' au début du tableau 'tabbis' , et recopier les lettres dans la partie restante de 'tabbis' ; 
-si nc < nl , ce sont les lettres qui devront être recopiées au début de 'tabbis' , et les chiffres dans la partie restante .
-Exemple : si 'tab' contient les caractères suivants : 
-A , 1 , 2, R , Y , 6 , F , 9 , 8 , 7  , le tableau 'tabbis' devra contenir : 
-1 , 2 , 6 , 9 , 8 , 7 , A , R , Y , F .
+a) Ecrire un programme qui demande à un utilisateur de donner N caractères qui sont soit des chiffres, soit des lettres, N étant un entier fourni par l'utilisateur au début de l'exécution du programme. Ces caractères seront stockés dans un tableau 'tab'. 
+  b) Ecrire un code qui permet de compter le nombre nc de chiffres contenus dans ce tableau.
+  c) Soit nl le nombre de lettres contenues dans le tableau 'tab'. 
+Ecrire un code pour recopier les données contenues dans 'tab' dans un tableau 'tabbis' selon la règle suivante: 
+si nc ≥ nl, recopier les chiffres de 'tab' au début du tableau 'tabbis', et recopier les lettres dans la partie restante de 'tabbis' ; 
+si nc < nl, ce sont les lettres qui devront être recopiées au début de 'tabbis', et les chiffres dans la partie restante.
+Exemple: si 'tab' contient les caractères suivants: 
+A, 1, 2, R, Y, 6, F, 9, 8, 7 , le tableau 'tabbis' devra contenir: 
+1, 2, 6, 9, 8, 7, A, R, Y, F.
 
-N.B : vous pouvez utiliser la méthode : Character.isDigit , qui prend en argument un caractère et retourne ‘true’ si le caractère est un nombre et ‘false’ dans le cas contraire , ainsi que la méthode : Character.isLetter , qui joue le même rôle pour les lettres . 
+N.B: vous pouvez utiliser la méthode: Character.isDigit, qui prend en argument un caractère et retourne ‘true’ 
+si le caractère est un nombre et ‘false’ dans le cas contraire, 
+ainsi que la méthode: Character.isLetter, qui joue le même rôle pour les lettres. 
  */
 
 public class NFA031_Devoir2_2 {
@@ -20,23 +22,23 @@ public class NFA031_Devoir2_2 {
 		int nombreChiffres;
 		String tableau;
 
-		System.out.print("Donner le nombre de characteres : ");
+		System.out.print("Donner le nombre de characteres: ");
 		nombreCharacteres = Lire.i();
 
 		char[] tab = remplirTableauCharacteres(nombreCharacteres);
 		tableau = convertTableauEnString(tab);
-		System.out.print("Le tableau de characteres est : ");
+		System.out.print("Le tableau de characteres est: ");
 
 		System.out.println(tableau);
 
 		nombreChiffres = calculerNombreChiffres(tab);
 		nombreLettres = calculerNombreLettres(tab);
 
-		System.out.println("Nb de chiffres : " + nombreChiffres);
-		System.out.println("Nb de lettres : " + nombreLettres);
+		System.out.println("Nb de chiffres: " + nombreChiffres);
+		System.out.println("Nb de lettres: " + nombreLettres);
 
 		char[] tabbis = creerTabbis(tab, nombreChiffres, nombreLettres);
-		System.out.print("Le tableau tabbis est : ");
+		System.out.print("Le tableau tabbis est: ");
 
 		System.out.println(convertTableauEnString(tabbis));
 
@@ -46,7 +48,7 @@ public class NFA031_Devoir2_2 {
 	public static char[] remplirTableauCharacteres(int nombreCharacteres) {
 		char[] tableauCharacteres = new char[nombreCharacteres];
 		for (int i = 0; i < tableauCharacteres.length; i++) {
-			System.out.print("Donner un charactere (lettre ou chiffre) : ");
+			System.out.print("Donner un charactere (lettre ou chiffre): ");
 			char characterIntroduit = Lire.c();
 			tableauCharacteres[i] = characterIntroduit;
 		}
@@ -105,21 +107,8 @@ public class NFA031_Devoir2_2 {
 		return nombreLettres;
 	}
 
-	// concatenner 2 tableaux
-	public static char[] concatTableaux1(char[] tab1, char[] tab2) {
-		char[] tab = new char[tab1.length + tab2.length];
-		for (int i = 0; i < tab1.length; i++) {
-			tab[i] = tab1[i];
-		}
-		int decalage = tab1.length;
-		for (int i = 0; i < tab2.length; i++) {
-
-			tab[i + decalage] = tab2[i];
-		}
-
-		return tab;
-	}
-
+	// concatenner 2 tableaux, en copiant un a un tous les éléments du tab1, puis du tab2, 
+	//dans la première position libre (qui sera incrémentée après chaque copie)
 	public static char[] concatTableaux(char[] tab1, char[] tab2) {
 		char[] tab = new char[tab1.length + tab2.length];
 		int positionLibreTab = 0;
@@ -136,13 +125,29 @@ public class NFA031_Devoir2_2 {
 		return tab;
 	}
 
+	// concatenner 2 tableaux (variante), en copiant à partir de la position 0 les valeurs du tableau tab1, 
+		// puis les valeurs du tab2
+		public static char[] concatTableaux1(char[] tab1, char[] tab2) {
+			char[] tab = new char[tab1.length + tab2.length];
+			for (int i = 0; i < tab1.length; i++) {
+				tab[i] = tab1[i];
+			}
+			int decalage = tab1.length;
+			for (int i = 0; i < tab2.length; i++) {
+
+				tab[i + decalage] = tab2[i];
+			}
+
+			return tab;
+		}
+	
+	// fonction pour créer le tableau tabbis
 	public static char[] creerTabbis(char[] tab, int nombreChiffres, int nombreLettres) {
-		// creez un tablou de cifre
+		// créer un tableau de chiffres
 		char[] tabChiffres = new char[nombreChiffres];
-		// creez un tablou de litere
+		// créer un tableau de lettres
 		char[] tabLettres = new char[nombreLettres];
-		// parcurg tab si umplu dintr-o singura trecere tablourile de cifre si
-		// de litere
+		// parcourir tab et remplir d'un seul passage les 2 tableaux de chiffres et lettres
 		int positionLibreTabChiffres = 0;
 		int positionLibreTabLettres = 0;
 		for (int i = 0; i < tab.length; i++) {
@@ -158,28 +163,26 @@ public class NFA031_Devoir2_2 {
 			}
 		}
 
-		// daca nrCifre > nrLitere
+		// si nombre de chiffres > nombre de lettres
 		char[] tabbis;
 		if (nombreChiffres > nombreLettres) {
-			// resultatul este concatenarea tabloului de cifre si a tabloului de
-			// litere
+			// le resultat est la concatenation du tableau de chiffres avec celui de lettres
 			tabbis = concatTableaux(tabChiffres, tabLettres);
 		} else {
-			// resultatul este concatenarea tabloului de litere si a tabloului
-			// de cifre
+			// le resultat est la concatenation du tableau de lettres avec celui de lettres
 			tabbis = concatTableaux(tabLettres, tabChiffres);
 		}
 
 		return tabbis;
 	}
 
+	// fonction pour créer le tableau tabbis (variante)
 	public static char[] creerTabbis1(char[] tab, int nombreChiffres, int nombreLettres) {
 		int longueurTabbis = nombreChiffres + nombreLettres;
 		char[] tabbis = new char[longueurTabbis];
 		if (nombreChiffres > nombreLettres) {
 			int positionLibre = 0;
-			// parcurg tablobul si cand gasesc o cifra o adaug in tabbis pe cea
-			// mai mica pozitie libera
+			// parcourir le tableau jusqu'un ciffre est trouvé. L'ajouter dans tabbis sur la plus petite position libre.
 			for (int i = 0; i < tab.length; i++) {
 				char temp = tab[i];
 				if (estChiffre(temp)) {
@@ -188,8 +191,7 @@ public class NFA031_Devoir2_2 {
 
 				}
 			}
-			// parcurg tablobul si cand gasesc o litera o adaug in tabbis la
-			// stanga
+			// parcourir le tableau jusqu'une lettre est trouvée. L'ajouter dans tabbis sur la plus petite position libre 
 			for (int i = 0; i < tab.length; i++) {
 				char temp = tab[i];
 				if (estLettre(temp)) {
@@ -200,8 +202,7 @@ public class NFA031_Devoir2_2 {
 			}
 		} else {
 			int positionLibre = 0;
-			// parcurg tablobul si cand gasesc o litera o adaug in tabbis pe cea
-			// mai mica pozitie libera
+			// parcourir le tableau jusqu'une lettre est trouvée. L'ajouter dans tabbis sur la plus petite position libre 
 			for (int i = 0; i < tab.length; i++) {
 				char temp = tab[i];
 				if (estLettre(temp)) {
@@ -210,8 +211,7 @@ public class NFA031_Devoir2_2 {
 
 				}
 			}
-			// parcurg tablobul si cand gasesc o cifre o adaug in tabbis la
-			// stanga
+			// parcourir le tableau jusqu'un ciffre est trouvé. L'ajouter dans tabbis sur la plus petite position libre.
 			for (int i = 0; i < tab.length; i++) {
 				char temp = tab[i];
 				if (estChiffre(temp)) {
