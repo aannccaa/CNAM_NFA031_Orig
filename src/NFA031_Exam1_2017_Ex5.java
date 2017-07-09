0c03a5fc4844602b5c4cfb69308cedaa0747e650
aannccaa <aannccaa@gmail.com>
2017-07-09 15:42:59 +0200

    ex 5

---
+++ src/NFA031_Exam1_2017_Ex5.java
@@ -0,0 +1,24 @@
+/*
+ Le responsable du parking d'une entreprise veut créer un programme pour
+archiver les codes d'accès au parking qu'il délivre aux salariés .
+Un code d'accès est composé de deux lettres majuscules , suivies de 9 chiffres et
+est terminé par une lettre majuscule .
+a) Rédiger une méthode pour vérifier qu'un code est , ou non , au bon format .
+b) Ecrire un code pour enregistrer dix codes dans un tableau 'Tab' , avec vérification
+du format de chaque code .
+c) Le responsable décide d'ajouter une clé à chaque code d'accès , constituée de la
+somme des chiffres du code modulo 11 . Compléter le programme pour qu'il rajoute
+à la fin de chaque code d'accès sa clé et pour que les codes d'accès ainsi complétés
+soient stockés dans un tableau 'Tabbis' .
+Remarque : pour obtenir la valeur numérique d'un caractère c représentant un chiffre , vous
+pouvez utiliser la syntaxe : Character.getNumericValue(c) .
+ */
+
+public class NFA031_Exam1_2017_Ex5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
