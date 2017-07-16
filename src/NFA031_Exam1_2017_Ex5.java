69131b225627cff147ac1e09b44e21956d7d490b
aannccaa <aannccaa@gmail.com>
2017-07-17 00:12:17 +0200

    #

--- src/NFA031_Exam1_2017_Ex5.java
+++ src/NFA031_Exam1_2017_Ex5.java
@@ -16,14 +16,20 @@ pouvez utiliser la syntaxe : Character.getNumericValue(c) .
 
 public class NFA031_Exam1_2017_Ex5 {
 
+	//public static String FORMAT = "XX000000000X";
 	public static String FORMAT = "XX00X";
-	public static String MSG_FORMAT = "2 maj 9 chiffres 1 maj";
-	
+	public static String MSG_FORMAT = "2 majuscules, 9 chiffres, 1 majuscule";
+
 	public static void main(String[] args) {
 		int nbCodes = 2;
 		String[] tab = creerTab(nbCodes);
+		String[] tabbis = creerTabbis(tab);
 
+		System.out.print("Voici tab : ");
 		afficherTab(tab);
+
+		System.out.print("Voici tabbis : ");
+		afficherTab(tabbis);
 	}
 
 	// introduire le code
@@ -32,10 +38,10 @@ public class NFA031_Exam1_2017_Ex5 {
 		boolean isCodeOK;
 
 		do {
-			System.out.print("Donner un code "+ MSG_FORMAT +"  : ");
+			System.out.print("Donner un code composé de " + MSG_FORMAT + "  : ");
 			code = Lire.S();
 			isCodeOK = verifierCode(code);
-			if (! isCodeOK) {
+			if (!isCodeOK) {
 				System.out.println("Code incorrect !");
 			}
 		} while (!isCodeOK);
@@ -43,65 +49,82 @@ public class NFA031_Exam1_2017_Ex5 {
 		return code;
 	}
 
-	public static boolean verifierCode(String code)  {
+	public static String introduireCodeClef() {
+		String code;
+		boolean isCodeOK;
+
+		do {
+			System.out.print("Donner un code " + MSG_FORMAT + "  : ");
+			code = Lire.S();
+
+			isCodeOK = verifierCode(code);
+			if (!isCodeOK) {
+				System.out.println("Code incorrect !");
+			}
+		} while (!isCodeOK);
+
+		return code;
+	}
+
+	// a) vérifier code
+	public static boolean verifierCode(String code) {
 		int formatLen = FORMAT.length();
-		if(code.length() != formatLen) {
+		if (code.length() != formatLen) {
 			return false;
 		}
-		for(int i = 0; i < formatLen; i++) {
+		for (int i = 0; i < formatLen; i++) {
 			char current = code.charAt(i);
 			char currentFormat = FORMAT.charAt(i);
-			switch(currentFormat) {
-				case 'X':
-					if(!Character.isAlphabetic(current) || !Character.isUpperCase(current)) {
-						return false;
-					}
-					break;
-				case '0':
-					if(!Character.isDigit(current)) {
-						return false;
-					}
-					break;
-				default:
-					System.out.println("Invalid format");
+			switch (currentFormat) {
+			case 'X':
+				if (!Character.isAlphabetic(current) || !Character.isUpperCase(current)) {
 					return false;
+				}
+				break;
+			case '0':
+				if (!Character.isDigit(current)) {
+					return false;
+				}
+				break;
+			default:
+				System.out.println("Invalid format");
+				return false;
 			}
 		}
 		return true;
 	}
 
-	public static boolean verifierCode2(String code)  {
+	public static boolean verifierCode2(String code) {
 		boolean isCodeOK = false;
 		char c;
-		if (
-				(code.length() == 5) 
-				&& (code.equals(code.toUpperCase()))) {
-					isCodeOK = true;
+		if ((code.length() == 5) && (code.equals(code.toUpperCase()))) {
+			isCodeOK = true;
 		}
-		if(isCodeOK) {
+		if (isCodeOK) {
 			for (int i = 0; i < 2; i++) {
 				c = code.charAt(i);
-				isCodeOK = isCodeOK &&  Character.isLetter(c);
+				isCodeOK = isCodeOK && Character.isLetter(c);
 			}
 		}
-			
-		if(isCodeOK) {
+
+		if (isCodeOK) {
 			for (int i = 2; i < 11; i++) {
 				c = code.charAt(i);
-				isCodeOK = isCodeOK &&  Character.isDigit(c);
+				isCodeOK = isCodeOK && Character.isDigit(c);
 			}
 		}
-		if(isCodeOK) {
+		if (isCodeOK) {
 			for (int i = 11; i < 12; i++) {
 				c = code.charAt(i);
-				isCodeOK = isCodeOK &&  Character.isLetter(c);
+				isCodeOK = isCodeOK && Character.isLetter(c);
 			}
 		}
 
 		return isCodeOK;
-	
+
 	}
-	// creer tab
+
+	// b) creer tab
 	public static String[] creerTab(int nbCodes) {
 		String[] tab = new String[nbCodes];
 
@@ -119,7 +142,33 @@ public class NFA031_Exam1_2017_Ex5 {
 			System.out.print(sep + tab[i]);
 			sep = "; ";
 		}
-		System.out.print("}");
+		System.out.println("}");
+	}
+
+	// calculer clef
+	public static int calculerClef(String code) {
+		int sommeChiffresCode = 0;
+		int clef;
+		for (int i = 0; i < code.length(); i++) {
+			char c = code.charAt(i);
+			if (Character.isDigit(c)) {
+				sommeChiffresCode = sommeChiffresCode + Character.getNumericValue(c);
+			}
+		}
+		clef = sommeChiffresCode % 11;
+		return clef;
+	}
+
+	// c) créer tabbis
+	public static String[] creerTabbis(String[] tab) {
+
+		String[] tabbis = new String[tab.length];
+
+		for (int i = 0; i < tabbis.length; i++) {
+			tabbis[i] = tab[i] + calculerClef(tab[i]);
+		}
+
+		return tabbis;
 	}
 
 }
